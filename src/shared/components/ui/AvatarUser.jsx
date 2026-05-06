9c81dedbdf3f7e4d22c7abcfb495c886121a1d32
mperez-2024357 <mperez-2024357@kinal.edu.gt>
2026-05-06 10:58:00 -0600

    feat: add AvatarUser component

--- src/shared/components/ui/AvatarUser.jsx
+++ src/shared/components/ui/AvatarUser.jsx
@@ -36,7 +36,7 @@ export const AvatarUser = () => {
     };
 
     const avatarSrc =
-        user?.profilePicture && user.profilePicture.trim() !== ""
+        user?.profilePicture && user.profilePicture.trim() !== "" && !user.profilePicture.includes("default-avatar_ewzxwx.png")
             ? user.profilePicture
             : defaultAvatarImg;
 
